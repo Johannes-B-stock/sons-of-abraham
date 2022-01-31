5d1a4cc6d8cbf8e2417792806e7a6992578d1b25
Johannes-B-stock <52247101+Johannes-B-stock@users.noreply.github.com>
2022-02-01 00:31:02 +0100

    Add level to footer to see how it looks like

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,90 @@
 <template>
-  <div>
-    <footer class="footer has-background-warning pb-6">
-      <cookie-law
-        :buttonText="$t('footer.cookieButton')"
-        buttonClass="button"
-        theme="dark-lime"
-      >
-        <div slot="message">
-          {{ $t('footer.cookie') }}
-        </div>
-      </cookie-law>
-      <div class="content has-text-centered">
-        <div class="navbar-item is-hidden-tablet">
-          <LanguagePicker up="true" />
-        </div>
-        <p>
-          Created by Johannes Birkenstock. Design and images by Rhythm DS.
-          &copy; {{ date }}
-          Sympony Of Nations (SoNs) all rights reserved
-        </p>
-
-        <p>
-          <router-link class="has-text-brown is-size-7 mx-5" to="/impressum"
-            >Impressum</router-link
-          >
-          <router-link class="has-text-brown is-size-7 mx-5" to="/datenschutz"
-            >Datenschutz</router-link
-          >
-        </p>
+  <footer class="footer has-background-warning pb-6">
+    <cookie-law
+      :buttonText="$t('footer.cookieButton')"
+      buttonClass="button"
+      theme="dark-lime"
+    >
+      <div slot="message">
+        {{ $t('footer.cookie') }}
       </div>
-    </footer>
-  </div>
+    </cookie-law>
+    <div class="container" style="margin-bottom:40px">
+      <nav class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <a href="http://www.sons-music.de/" target="blank">
+              <img src="../assets/SONS-logo-BW.svg" alt="SoNs" width="150px" />
+            </a>
+          </div>
+        </div>
+        <div class="level-item">
+          <router-link class="navbar-item" to="/" exact>{{
+            $t('header.about')
+          }}</router-link>
+          <router-link class="navbar-item" to="/music">{{
+            $t('header.music')
+          }}</router-link>
+          <router-link class="navbar-item" to="/contact">{{
+            $t('header.contact')
+          }}</router-link>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            <span class="icon icons has-text-success" style="font-size: 1.5em">
+              <a
+                href="https://www.facebook.com/SoNs.Symphony/"
+                class=""
+                target="blank"
+              >
+                <i class="fab fa-facebook"></i>
+              </a>
+            </span>
+          </div>
+          <div class="level-item">
+            <span class="icon icons has-text-info" style="font-size: 1.5em">
+              <a
+                href="https://open.spotify.com/album/0nVq2RGIw3WxL57GBUPEA1?si=XPPqUsJTRka3tnd-Aq8U_Q&dl_branch=1"
+                target="blank"
+              >
+                <i class="fab fa-spotify"></i>
+              </a>
+            </span>
+          </div>
+          <div class="level-item">
+            <span class="icon icons has-text-info" style="font-size: 1.5em">
+              <a
+                href="https://geo.music.apple.com/us/album/sons-of-abraham/1563538824?itsct=music_box_link&itscg=30200&ct=albums_so_ns_of_abraham&ls=1&app=music"
+                target="blank"
+              >
+                <i class="fab fa-itunes"></i>
+              </a>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="has-text-centered is-size-7">
+      <div class="navbar-item is-hidden-tablet">
+        <LanguagePicker up="true" />
+      </div>
+      <p>
+        Created by Johannes Birkenstock. Design and images by Rhythm DS. &copy;
+        {{ date }}
+        Sympony Of Nations (SoNs) all rights reserved
+      </p>
+
+      <p>
+        <router-link class="has-text-brown is-size-7 mx-5" to="/impressum"
+          >Impressum</router-link
+        >
+        <router-link class="has-text-brown is-size-7 mx-5" to="/datenschutz"
+          >Datenschutz</router-link
+        >
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
